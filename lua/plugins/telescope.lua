7b9464a6a3c2a6fbc0d4ac6b5a2e26e82e6e99ae
iikkjjll <iikkjjll@gitee.com>
2023-07-14 02:50:29 +0800

    added telescope keymaps.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
-	cmd = "Telescope",
 	'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	cmd = "Telescope",
 	-- or                              , branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim' },
+	keys = {
+		{"<leader>ff", "<cmd>Telescope find_files<Cr>", desc = "find files"},
+		{"<leader>fh", "<cmd>Telescope help_tags<Cr>", desc = "find help"},
+		{"<leader>fg", "<cmd>Telescope live_grep<Cr>", desc = "find content"},
+		{"<leader>fb", "<cmd>Telescope live_grep<Cr>", desc = "find buffers"},
+	}
 }
