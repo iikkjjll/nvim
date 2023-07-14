eb4943f223dc3030f10c4df4a9cadc5bc476176f
iikkjjll <iikkjjll@gitee.com>
2023-07-15 00:47:54 +0800

    bug fix: markdown-preview plugin start by filetype and keys. But now, just start by filetype.
    now, using: ,b | ,, | ,i | ,s | and so on. It's not working for other filetype expect markdown.

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -19,17 +19,28 @@ return {
         disable_filename = 1,
         toc = {}
 	}
+-- set keymaps for markdown filetype
+    vim.keymap.set('n', '<leader>r', "<cmd>MarkdownPreviewToggle<cr>", {noremap = true, silent = true})
+    vim.keymap.set('i', ',,', "<++>", {noremap = true, silent = true})
+    vim.keymap.set('i', ',f', "<Esc>/<++><CR>:nohlsearch<CR>c4l", {noremap = true, silent = true})
+    vim.keymap.set('n', ',f', "<Esc>/<++><CR>:nohlsearch<CR>c4l", {noremap = true, silent = true})
+    vim.keymap.set('i', ',i', "** <++><Esc>F*i", {noremap = true, silent = true})
+    vim.keymap.set('i', ',b', "**** <++><Esc>F*hi", {noremap = true, silent = true})
+    vim.keymap.set('i', ',n', "---<CR>", {noremap = true, silent = true})
+    vim.keymap.set('i', ',s', "~~~~ <++><Esc>F~hi", {noremap = true, silent = true})
+    vim.keymap.set('i', ',d', "``<++><Esc>F`i", {noremap = true, silent = true})
+    vim.keymap.set('i', ',c', "```<CR><++><CR>```<CR><++><Esc>kkkA", {noremap = true, silent = true})
   end,
-  keys = {
-	  {"<leader>r", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle preview window in normal mode", mode = "n" },
-	  {",,", "<++>", desc = "default tamplate", mode = "i" },
-	  {",f", "<Esc>/<++><CR>:nohlsearch<CR>c4l", desc = "find next in insert mode", mode = "i" },
-	  {",f", "<Esc>/<++><CR>:nohlsearch<CR>c4l", desc = "find next in normal mode", mode = "n" },
-	  {",i", "** <++><Esc>F*i", desc = "italic font", mode = "i" },
-	  {",b", "**** <++><Esc>F*hi", desc = "bold font", mode = "i" },
-	  {",n", "---<CR>", desc = "draw a line", mode = "i" },
-	  {",s", "~~~~ <++><Esc>F~hi", desc = "delete line on font", mode = "i" },
-	  {",d", "``<++><Esc>F`i", desc = "inline code", mode = "i" },
-	  {",c", "```<CR><++><CR>```<CR><++><Esc>kkkA", desc = "block code", mode = "i" },
-  },
+  -- keys = {
+	 --  {"<leader>r", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle preview window in normal mode", mode = "n" },
+	 --  {",,", "<++>", desc = "default tamplate", mode = "i" },
+	 --  {",f", "<Esc>/<++><CR>:nohlsearch<CR>c4l", desc = "find next in insert mode", mode = "i" },
+	 --  {",f", "<Esc>/<++><CR>:nohlsearch<CR>c4l", desc = "find next in normal mode", mode = "n" },
+	 --  {",i", "** <++><Esc>F*i", desc = "italic font", mode = "i" },
+	 --  {",b", "**** <++><Esc>F*hi", desc = "bold font", mode = "i" },
+	 --  {",n", "---<CR>", desc = "draw a line", mode = "i" },
+	 --  {",s", "~~~~ <++><Esc>F~hi", desc = "delete line on font", mode = "i" },
+	 --  {",d", "``<++><Esc>F`i", desc = "inline code", mode = "i" },
+	 --  {",c", "```<CR><++><CR>```<CR><++><Esc>kkkA", desc = "block code", mode = "i" },
+  -- },
 }
